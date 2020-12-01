cfd213686c15ec04cdc754bb2639ee3a3ee44b14
Evgenii Timofeev <nawasington@ya.ru>
2020-12-01 15:51:14 +0300

    add models

--- pik_test_task/suppliers/models.py
+++ pik_test_task/suppliers/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+    class Meta:
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+
+    name = models.CharField("Название организации", max_length=255, unique=True)
+    email = models.CharField(max_length=255)
+    phone = models.CharField("Номер телефона", max_length=20)
+    address = models.TextField("Адрес центрального офиса")
+
+
+class ServiceTypes(models.Model):
+    class Meta:
+        verbose_name = "Тип услуг"
+        verbose_name_plural = "Типы услуг"
+
+    name = models.CharField("Название", max_length=255, unique=True)
+
+
+class ServiceArea(models.Model):
+    class Meta:
+        verbose_name = "Область обслуживания"
+        verbose_name_plural = "Области обслуживания"
+
+    supplier = models.ForeignKey("Supplier", on_delete=models.CASCADE)
+    name = models.CharField("Название", max_length=255, unique=True)
+    price = models.IntegerField("цена обслуживания за единицу оказываемой услуги")
+    service_types = models.ManyToManyField(ServiceTypes)
+    geometry = models.TextField("Область")
