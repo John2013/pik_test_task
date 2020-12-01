9315c73c9360a7266dcf9047a7b36a8e1d052456
Evgenii Timofeev <nawasington@ya.ru>
2020-12-01 17:50:59 +0300

    views

--- pik_test_task/suppliers/views.py
+++ pik_test_task/suppliers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Supplier, ServiceArea, ServiceType
+from .serializers import (
+    SupplierSerializer,
+    ServiceAreaSerializer,
+    ServiceTypeSerializer,
+)
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    queryset = ServiceArea.objects.all()
+    serializer_class = ServiceAreaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ServiceTypeViewSet(viewsets.ModelViewSet):
+    queryset = ServiceType.objects.all()
+    serializer_class = ServiceTypeSerializer
+    permission_classes = [permissions.IsAuthenticated]
