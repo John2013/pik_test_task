aba5107322d43a81d13781fb27fbf7882df8aeb2
Evgenii Timofeev <nawasington@ya.ru>
2020-12-04 04:08:28 +0300

    serializers settings

--- pik_test_task/suppliers/serializers.py
+++ pik_test_task/suppliers/serializers.py
@@ -1,23 +1,33 @@
 from rest_framework import serializers
 
-from .models import Supplier, ServiceArea, ServiceType
+from .models import Supplier, ServiceArea, ServiceType, TypeInArea
 
 
 class SupplierSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Supplier
-        fields = ["name", "email", "phone", "address", "servicearea_set"]
+        fields = ["id", "name", "email", "phone", "address", "servicearea_set"]
 
 
 class ServiceTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ServiceType
-        fields = ["name"]
+        fields = ["id", "name"]
+
+
+class TypeInAreaSerializer(serializers.HyperlinkedModelSerializer):
+    area_name = serializers.StringRelatedField(source="area")
+    type_name = serializers.StringRelatedField(source="service_type")
+
+    class Meta:
+        model = TypeInArea
+        fields = ["area", "area_name", "service_type", "type_name", "price"]
 
 
 class ServiceAreaSerializer(serializers.HyperlinkedModelSerializer):
-    servicetypes = ServiceTypeSerializer(many=True)
+    types = serializers.StringRelatedField(many=True)
+    typeinarea_set = TypeInAreaSerializer(many=True, read_only=True)
 
     class Meta:
         model = ServiceArea
-        fields = ["supplier", "name", "price", "servicetypes", "geometry"]
+        fields = ["id", "supplier", "name", "types", "typeinarea_set", "geometry"]
