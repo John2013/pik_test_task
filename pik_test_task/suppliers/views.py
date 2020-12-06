3ae8a46504794e263c8ecc6acf4a053c538b9c3e
Evgenii Timofeev <nawasington@ya.ru>
2020-12-06 19:31:02 +0300

    added in_point action

--- pik_test_task/suppliers/views.py
+++ pik_test_task/suppliers/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from .models import Supplier, ServiceArea, ServiceType, TypeInArea
 from .serializers import (
     SupplierSerializer,
     ServiceAreaSerializer,
-    ServiceTypeSerializer, TypeInAreaSerializer,
+    ServiceTypeSerializer,
+    TypeInAreaSerializer,
 )
 
 
@@ -12,6 +16,26 @@ class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False)
+    def in_point(self, request: Request):
+        query_params = tuple(map(request.query_params.get, ("lat", "lon")))
+        if None in query_params:
+            return Response(
+                {"message": "query params `lat` and `lon` must be set"},
+                status=status.HTTP_400_BAD_REQUEST,
+                exception=ValueError("query params `lat` and `lon` must be set"),
+            )
+        try:
+            latitude, longitude = tuple(map(float, query_params))
+        except ValueError as error:
+            return Response(
+                {"message": "query params `lat` and `lon` must be float"},
+                status=status.HTTP_400_BAD_REQUEST,
+                exception=error,
+            )
+
+        return Response({"latitude": latitude, "longitude": longitude})
 
 
 class ServiceAreaViewSet(viewsets.ModelViewSet):
