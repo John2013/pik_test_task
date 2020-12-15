66f3e8d5afa5ea6819df6e621c999655c869767f
Evgenii Timofeev <nawasington@ya.ru>
2020-12-16 01:52:42 +0300

    requests tests

--- pik_test_task/suppliers/tests.py
+++ pik_test_task/suppliers/tests.py
@@ -1,54 +1,47 @@
 import random
+from typing import Iterable
+import http
 
 from django.contrib.auth.models import User
 from django.test import TestCase
 from faker import Faker
 from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
 
-from pik_test_task.suppliers.models import Supplier, ServiceType
-
+from pik_test_task.suppliers.models import Supplier, ServiceType, ServiceArea
 
 POLYGON_TEMPLATE = """
 {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              {lat1},
-              {lon1}
-            ],
-            [
-              {lat2},
-              {lon2}
-            ],
-            [
-              {lat3},
-              {lon3}
-            ],
-            [
-              {lat4},
-              {lon4}
-            ],
-            [
-              {lat1},
-              {lon1}
-            ]
-          ]
-        ]
-      }
-    }
+  "type": "Polygon",
+  "coordinates": [
+    [
+      [
+        {lat1},
+        {lon1}
+      ],
+      [
+        {lat2},
+        {lon2}
+      ],
+      [
+        {lat3},
+        {lon3}
+      ],
+      [
+        {lat4},
+        {lon4}
+      ],
+      [
+        {lat1},
+        {lon1}
+      ]
+    ]
   ]
-}
+}  
 """
 
 
-def create_test_suppliers():
+def create_test_suppliers(service_types: Iterable[ServiceType]):
     fake = Faker()
     Faker.seed(0)
 
@@ -84,7 +77,11 @@ def create_test_suppliers():
                 lon4=lon4,
             )
 
-            supplier.servicearea_set.create(name=fake.unique.company(), geometry=polygon)
+            supplier.servicearea_set.create(
+                name=fake.unique.company(), geometry=polygon, types=service_types
+            )
+        suppliers.append(supplier)
+    return suppliers
 
 
 class SuppliersTest(TestCase):
@@ -93,7 +90,7 @@ class SuppliersTest(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def test_creating_suppliers(self):
+    def test_creating_supplier(self):
         response = self.client.post(
             "/suppliers/",
             {
@@ -101,11 +98,69 @@ class SuppliersTest(TestCase):
                 "email": "test@test.ru",
                 "phone": "+79910555500",
                 "address": "address",
-                "servicearea_set": "[]",
+                "servicearea_set": [],
             },
         )
+        self.assertEqual(response.status_code, http.HTTPStatus.CREATED)
+        supplier = Supplier.objects.last()
+        self.assertEqual(supplier.name, "test")
+
+    def test_creating_type(self):
+        response = self.client.post("/types/", {"name": "type1"})
+        self.assertEqual(response.status_code, http.HTTPStatus.CREATED)
+        type = ServiceType.objects.last()
+        self.assertEqual(type.name, "type1")
+
+    def test_creating_area(self):
+        fake = Faker()
+        Faker.seed(0)
+
+        supplier = Supplier.objects.create(
+            name=fake.unique.company(),
+            email=fake.unique.email(),
+            phone=fake.unique.phone_number(),
+            address=fake.unique.address(),
+        )
+        response = self.client.post(
+            "/areas/",
+            {
+                "supplier": reverse("supplier-detail", [supplier.pk]),
+                "name": "test",
+                "types": [],
+                "geometry": (
+                    "{\n"
+                    '  "type": "Polygon",\n'
+                    '  "coordinates": [\n'
+                    "    [\n"
+                    "      [\n"
+                    "        37.901201248168945,\n"
+                    "        59.088825146044556\n"
+                    "      ],\n"
+                    "      [\n"
+                    "        37.918453216552734,\n"
+                    "        59.088825146044556\n"
+                    "      ],\n"
+                    "      [\n"
+                    "        37.918453216552734,\n"
+                    "        59.09596730040326\n"
+                    "      ],\n"
+                    "      [\n"
+                    "        37.901201248168945,\n"
+                    "        59.09596730040326\n"
+                    "      ],\n"
+                    "      [\n"
+                    "        37.901201248168945,\n"
+                    "        59.088825146044556\n"
+                    "      ]\n"
+                    "    ]\n"
+                    "  ]\n"
+                    "}"
+                ),
+            },
+        )
+        self.assertEqual(response.status_code, http.HTTPStatus.CREATED, response.json())
+        area = ServiceArea.objects.last()
+        self.assertEqual(area.name, "test")
 
     def test_returning_suppliers_with_area_in_point(self):
-        user = User.objects.create_user(username="test_user")
-        client = APIClient()
-        client.force_authenticate(user=user)
+        pass
