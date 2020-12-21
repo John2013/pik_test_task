92ff220a89cf1fbd85bd81fd0b5925b232d60b7c
Evgenii Timofeev <nawasington@ya.ru>
2020-12-21 20:51:55 +0300

    in_point action changed to queryset filter

--- pik_test_task/suppliers/views.py
+++ pik_test_task/suppliers/views.py
@@ -1,7 +1,7 @@
+from typing import Optional
+
 from django.contrib.gis.geos import Point
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import action
-from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .models import Supplier, ServiceArea, ServiceType, TypeInArea
@@ -18,32 +18,25 @@ class SupplierViewSet(viewsets.ModelViewSet):
     serializer_class = SupplierSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False)
-    def in_point(self, request: Request):
-        query_params = tuple(map(request.query_params.get, ("lat", "lon")))
-        if None in query_params:
-            return Response(
-                {"message": "query params `lat` and `lon` must be set"},
-                status=status.HTTP_400_BAD_REQUEST,
-                exception=ValueError("query params `lat` and `lon` must be set"),
-            )
-        try:
-            latitude, longitude = tuple(map(float, query_params))
-        except ValueError as error:
-            return Response(
-                {"message": "query params `lat` and `lon` must be float"},
-                status=status.HTTP_400_BAD_REQUEST,
-                exception=error,
-            )
-        point = Point((latitude, longitude))
-        suppliers = self.queryset.filter(servicearea__geometry__contains=point)
-        page = self.paginate_queryset(suppliers)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+    def get_queryset(self):
+        queryset = Supplier.objects.all()
+        # noinspection PyTypeChecker
+        point_query_params: tuple[Optional[str]] = tuple(
+            map(self.request.query_params.get, ("lat", "lon"))
+        )
+        if None not in point_query_params:
+            try:
+                latitude, longitude = tuple(map(float, point_query_params))
+            except ValueError as error:
+                return Response(
+                    {"message": "query params `lat` and `lon` must be float"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                    exception=error,
+                )
+            point = Point((latitude, longitude))
+            queryset = queryset.filter(servicearea__geometry__contains=point)
 
-        serializer = self.get_serializer(suppliers, many=True)
-        return Response(serializer.data)
+        return queryset
 
 
 class ServiceAreaViewSet(viewsets.ModelViewSet):
